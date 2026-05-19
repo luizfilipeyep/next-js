10704e1277b9d472792e8a36890b6c7b366dcd65
Luiz Omena <luizfilipeyep@gmail.com>
2026-05-19 05:18:20 -0300

    Server Actions
    Introduce server-side actions to curso/src/app/contatos/page.tsx: add handleClickButton and handleSearchUsers marked with "use server" and wired into the UI via a button and a form (action=handleSearchUsers). The form reads userId from FormData and both handlers use console.log for simple debugging.

--- curso/src/app/contatos/page.tsx
+++ curso/src/app/contatos/page.tsx
@@ -1,3 +1,6 @@
+import { log } from "next/dist/server/typescript/utils";
+
+
 interface PostProps {
   id: number;
   title: string;
@@ -13,11 +16,33 @@ export default async function Contatos() {
   const response = await fetch("https://dummyjson.com/posts");
   const data: ResponseProps = await response.json()
 
+  
+
+  const handleClickButton = async () => {
+    "use server"
+
+    console.log("Clicou");
+  }
+
+  const handleSearchUsers = async (formData: FormData) => {
+    "use server"
+
+    const userId = formData.get("userId")
+    console.log(`Buscando o usuário: ${userId}`);
+  }
+
   console.log(data);
 
   return (
     <>
       <h1>Página CONTATOS</h1>
+
+      <button onClick={handleClickButton}>Usando lado servidor</button>
+
+      <form action={handleSearchUsers}>
+        <input type="text" placeholder="Nome do usuário" name="userId" />
+        <input type="submit" value="Buscar" />
+      </form>
 
       <div>
         {
